0957b4f20b4427a0bd3a133fcb56ca30ade10a19
hongchunhua <hongchunhua@ruijie.com.cn>
2020-08-28 20:32:49 +0800

    oth:关闭debug日志
    Change-Id: I46dda1574cc580b06c4e47a90ef58f1cfa92b7fc

--- src/common/base_log.h
+++ src/common/base_log.h
@@ -29,12 +29,12 @@ extern "C" {
 #define BASE_ERROR			-1
 #define BASE_SUCCESS		 0
 
-#define BASE_DEBUG_ON
-#define BASE_LOG_ERROR(format, arg...) fprintf(stdout, 	"[ arpc ] [ ERROR ] %s|%d |File:%s---"format"\n",__FUNCTION__, __LINE__, __FILE__, ##arg)
-#define BASE_LOG_NOTICE(format, arg...) fprintf(stdout, "[ arpc ] [ NOTICE ] %s|%d |File:---"format"\n",__FUNCTION__, __LINE__, ##arg)
+//#define BASE_DEBUG_ON
+#define BASE_LOG_ERROR(format, arg...) fprintf(stdout, 	"[  ARPC] [  ERROR] file:%s func:%s|%d---"format"\n", __FILE__, __FUNCTION__, __LINE__,##arg)
+#define BASE_LOG_NOTICE(format, arg...) fprintf(stdout, "[  ARPC] [ NOTICE] func:%s|%d---"format"\n",__FUNCTION__, __LINE__, ##arg)
 
 #ifdef BASE_DEBUG_ON
-#define BASE_LOG_DEBUG(format, arg...) fprintf(stdout, "[ arpc ] [ DEBUG ] %s|%d |File: ---"format"\n",__FUNCTION__, __LINE__, ##arg)
+#define BASE_LOG_DEBUG(format, arg...) fprintf(stdout, "[ ARPC ] [ DEBUG ] func:%s|%d---"format"\n",__FUNCTION__, __LINE__, ##arg)
 #else
 #define BASE_LOG_DEBUG(format, arg...)
 #endif
