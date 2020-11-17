2e412ff785205bf9e5ab44af5dc5c80a026bd2f6
hongchunhua <hongchunhua@ruijie.com.cn>
2020-11-16 22:00:52 -0500

    feat:增加日志模块开关
    Change-Id: I77f9bd211054626f58b7e425b3840f39c80a1c3c

--- src/common/base_log.c
+++ src/common/base_log.c
@@ -30,9 +30,111 @@ extern "C" {
 
 #define LOG_TIME_FMT "%04d/%02d/%02d-%02d:%02d:%02d.%05ld"
 
-pid_t gettid()
+#define LOG_SWITCH_INIT (1<<ARPC_LOG_LEVEL_E_MAX)
+#define LOG_TRACE_SWICH_FILE "on_trace"
+#define LOG_DEBUG_SWICH_FILE "on_debug"
+#define LOG_INFO_SWICH_FILE  "on_info"
+#define LOG_ERROR_SWICH_FILE "off_err"
+#define LOG_FATAL_SWICH_FILE "off_info"
+
+//syslog
+#define SYSLOG_SWICH_ENABLE (1<<(ARPC_LOG_LEVEL_E_MAX + 1))
+#define SYSLOG_SWICH_FILE  "on_syslog"
+
+//fprintf
+#define FPRINTF_SWICH_ENABLE (1<<(ARPC_LOG_LEVEL_E_MAX + 2))
+#define FPRINTF_SWICH_FILE "on_fprintf"
+
+//本地文件
+#define LOG_FILE_ENABLE (1<<(ARPC_LOG_LEVEL_E_MAX + 3))
+#define LOG_FILE_SWICH_FILE  "on_filelog"
+#define LOG_FILE_NAME        "messages.log"
+#define LOG_FILE_MAX_SIZE    (16*1024)
+
+
+static void set_log_switch(const char *module, int32_t *log_enabale)
+{
+    char			buf[128];
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_TRACE_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|(1<<ARPC_LOG_LEVEL_E_TRACE);
+    }
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_DEBUG_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|(1<<ARPC_LOG_LEVEL_E_DEBUG);
+    }
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_INFO_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|(1<<ARPC_LOG_LEVEL_E_INFO);
+    }
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_ERROR_SWICH_FILE);
+    if(access(buf, F_OK) != 0){
+        *log_enabale = (*log_enabale)|(1<<ARPC_LOG_LEVEL_E_ERROR);
+    }
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_FATAL_SWICH_FILE);
+    if(access(buf, F_OK) != 0){
+        *log_enabale = (*log_enabale)|(1<<ARPC_LOG_LEVEL_E_FATAL);
+    }
+
+	snprintf(buf, sizeof(buf), "/run/%s/%s", module, SYSLOG_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|SYSLOG_SWICH_ENABLE;
+    }
+
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, FPRINTF_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|FPRINTF_SWICH_ENABLE;
+    }
+
+    snprintf(buf, sizeof(buf), "/run/%s/%s", module, LOG_FILE_SWICH_FILE);
+    if(access(buf, F_OK) == 0){
+        *log_enabale = (*log_enabale)|LOG_FILE_ENABLE;
+    }
+
+    *log_enabale = (*log_enabale)|LOG_SWITCH_INIT;
+}
+
+static pid_t gettid()
 {
 	return syscall(SYS_gettid);
+}
+
+static long get_file_size(char* filename)
+{
+	long length = 0;
+	FILE *fp = NULL;
+
+	fp = fopen(filename, "rb");
+	if (fp != NULL){
+		fseek(fp, 0, SEEK_END);
+		length = ftell(fp);
+        fclose(fp);
+	}
+	return length;
+}
+
+static void write_log_file(char* filename, long max_size, const char *fmt, ...)
+{
+	FILE *fp;
+    long length;
+    va_list			args;
+	char			buf[2048];
+    if (filename != NULL){
+		length = get_file_size(filename);
+		if (length > max_size){
+			unlink(filename); // 删除文件
+		}
+		fp = fopen(filename, "at+");
+        if (fp != NULL){
+            va_start(args, fmt);
+            length = vsnprintf(buf, sizeof(buf), fmt, args);
+            va_end(args);
+            buf[length] = 0;
+            fwrite(buf, length, 1, fp);
+            fclose(fp);
+            fp = NULL;
+        }
+	}
 }
 
 void arpc_vlog(enum arpc_log_level level, const char *module, const char *file,unsigned line, const char *function, const char *fmt, ...)
@@ -43,11 +145,24 @@ void arpc_vlog(enum arpc_log_level level, const char *module, const char *file,u
 	struct tm		t;
 	char			buf[2048];
 	char			buf2[256];
+	char            log_file_path[64];
 	int			length = 0;
+	static int32_t log_enabale = 0;
 	static const char * const level_str[ARPC_LOG_LEVEL_E_MAX] = {
 		"FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"
 	};
 	time_t time1;
+
+	if (log_enabale&LOG_SWITCH_INIT) {
+        if(!(log_enabale&(1<<level))){
+            return;
+        }
+    }else{
+        set_log_switch(module, &log_enabale);
+		if(!(log_enabale&(1<<level))){
+            return;
+        }
+    }
 
 	va_start(args, fmt);
 	length = vsnprintf(buf, sizeof(buf), fmt, args);
@@ -66,19 +181,32 @@ void arpc_vlog(enum arpc_log_level level, const char *module, const char *file,u
 		snprintf(buf2, sizeof(buf2), "%s:%u", short_file, line);
 	}
 
-	fprintf(stderr, "[%-5s][%-5x][%-5s] [" LOG_TIME_FMT "] %-24s - %s\n", module, gettid(),
-		level_str[level],
-		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
-		t.tm_hour, t.tm_min, t.tm_sec, tv.tv_usec,
-		buf2,
-		buf);
-	syslog(LOG_ERR, "[%-5s][%-5x][%-5s] [" LOG_TIME_FMT "] %-24s - %s", module, gettid(),
-		level_str[level],
-		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
-		t.tm_hour, t.tm_min, t.tm_sec, tv.tv_usec,
-		buf2,
-		buf);
+	if (log_enabale&FPRINTF_SWICH_ENABLE) {
+		fprintf(stderr, "[%-5s][%-5x][%-5s] [" LOG_TIME_FMT "] %-24s - %s\n", module, gettid(),
+			level_str[level],
+			t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+			t.tm_hour, t.tm_min, t.tm_sec, tv.tv_usec,
+			buf2,
+			buf);
+	}
+	if (log_enabale&SYSLOG_SWICH_ENABLE) {
+		syslog(LOG_ERR, "[%-5s][%-5x][%-5s] [" LOG_TIME_FMT "] %-24s - %s", module, gettid(),
+			level_str[level],
+			t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+			t.tm_hour, t.tm_min, t.tm_sec, tv.tv_usec,
+			buf2,
+			buf);
+	}
 
+	if (log_enabale&LOG_FILE_ENABLE) {
+        snprintf(log_file_path, sizeof(log_file_path), "/run/%s/%s", module, LOG_FILE_NAME);
+        write_log_file(log_file_path, LOG_FILE_MAX_SIZE, "[%-5s][%-5x][%-5s] [" LOG_TIME_FMT "] %-24s - %s\n", module, gettid(),
+		level_str[level],
+		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+		t.tm_hour, t.tm_min, t.tm_sec, tv.tv_usec,
+		buf2,
+		buf);
+    }
 	//fflush(stderr);
 }
 
